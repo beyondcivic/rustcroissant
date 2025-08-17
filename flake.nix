15c556bde1b86a0c5ee27935b413717485dd5aa4
alvaradojl <jorge.alvarado@beyondcivic.com>
2025-08-17 17:40:18 +0200

    added ci/cd

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "Rust development environment using Nix with naersk";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
