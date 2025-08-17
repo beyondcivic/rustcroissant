a78595c594d3d598bfe24c360cdf5d911cd15e3c
alvaradojl <jorge.alvarado@beyondcivic.com>
2025-08-17 20:07:50 +0200

    now prints version from build

--- src/version/version_info.rs
+++ src/version/version_info.rs
@@ -1,25 +1,19 @@
 //! Version information module for rustcroissant
 
+// Include generated version info from build.rs
+include!(concat!(env!("OUT_DIR"), "/version_info.rs"));
+
 /// Struct containing application version information
 pub struct Version {
     /// Name of the application
     pub app_name: &'static str,
-
     /// Service version
     pub version: &'static str,
-
     /// Git hash of the commit the service is built from
     pub git_hash: &'static str,
-
     /// Build time in RFC3339 format
     pub build_time: &'static str,
 }
-
-// Default values - will be overridden during build
-pub static APP_NAME: &str = "rustcroissant";
-pub static VERSION: &str = "dev";
-pub static GIT_HASH: &str = "dev";
-pub static BUILD_TIME: &str = "now";
 
 /// Get the current version information
 pub fn get_version() -> Version {
